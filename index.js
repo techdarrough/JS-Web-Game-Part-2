c53d07a7505772412d0f33f9169450229967be7f
Preston Darrough <techdarrough@gmail.com>
2022-01-18 19:55:32 -0800

    project completion

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -9,26 +10,34 @@ function newImage(url, left, bottom){
 }
 
 function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-}
+    let item = newImage(url, left, bottom);
+    item.addEventListener('click', function() {item.remove()})
+    let inventoryItem = document.createElement('img')
+    inventoryItem.src = url
+    inventory.append(inventoryItem)
+};
+
+    
 
 function newInventory(){
-    let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
+    let localInventory = document.createElement('div')
+    localInventory.style.position = 'fixed'
+    localInventory.style.bottom = '0px';
+    localInventory.style.left = '0px'
+    localInventory.style.width = '100%'
+    localInventory.style.height = '100px'
+    localInventory.style.display = 'flex'
+    localInventory.style.flexDirection = 'row'
+    localInventory.style.alignItems = 'center'
+    localInventory.style.justifyContent = 'space-evenly'
+    localInventory.style.border = '2px solid black'
+    localInventory.style.backgroundColor = 'brown'
+    document.body.append(localInventory)
+    return localInventory    
 }
+let inventory = newInventory()
 
-newInventory()
+// newInventory()
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
